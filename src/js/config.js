5d74652b2e8f3f620fea29c5a245fe2e2953006b
YuliyaYurevich <yurevich@mail.com>
2021-05-13 16:34:09 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c2e98d1f-e375-48db-b9d9-f97d37dacdd8';
+export const MODAL_CLOSE_SEC = 2.5;
